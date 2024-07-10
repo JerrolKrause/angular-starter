443e8a0f9a681ec816aee17ebfeda301855f1f04
Matt Rossetta <matt@l5ui.com>
2024-07-10 09:30:49 -0400

    Linking and change detection optimizations

--- src/app/components/masterpage/footer/footer.component.ts
+++ src/app/components/masterpage/footer/footer.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   styleUrls: ['./footer.component.scss'],
   templateUrl: './footer.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FooterComponent {
   constructor() {}
