bf0fc3b3481069fa738c0a21f1ccceec1f2e360c
Matt Rossetta <matt@l5ui.com>
2024-07-10 12:54:46 -0400

    Type safe environment files and synced file names with angular.json build commands

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,13 +1,14 @@
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// `ng build --env=development` then `environment.development.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 import { mergeDeepRight } from 'ramda';
 import { environment as defaults } from './environment.defaults';
+import { EnvironmentConfig } from './environment.model';
 
 /**
  * Dev env config with local override
  */
-export const environment = mergeDeepRight(defaults, {
-  prod: true,
+export const environment: EnvironmentConfig = mergeDeepRight<EnvironmentConfig, Partial<EnvironmentConfig>>(defaults, {
+  production: false,
 });
